1fb0973ac49f8e00d4c9e71ea2bced448221e630
Nekvall <nekvallgames@gmail.com>
2016-04-01 18:12:27 +0400

    Реализовал рисование мышкой

--- Assets/Scripts/GameScreen/Manager/DrawManager.cs
+++ Assets/Scripts/GameScreen/Manager/DrawManager.cs
@@ -1,22 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Менеджер, который отрисовывает фигуру, котору в настоящее время рисует игрок.
 /// </summary>
 public class DrawManager : MonoBehaviour 
 {
+    private bool _drawLine;
+    private List<Vector3> _pointsList;
+    
+    private LineRenderer _line;
+    private Vector3 mousePos;
 
-	// Use this for initialization
-	void Start () 
+    void Awake() 
 	{
-	
+        _drawLine = false;
+        _pointsList = new List<Vector3>();
+        _line = gameObject.AddComponent<LineRenderer>();
+        _line.SetVertexCount(0);
+        _line.SetWidth(0.2f, 0.2f);
+        _line.SetColors(Color.green, Color.green);
+        _line.useWorldSpace = true;
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-	
+        if (!_drawLine) return;
+ 
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        
+        _pointsList.Add(mousePos);
+
+        _line.SetVertexCount(_pointsList.Count);
+        _line.SetPosition(_pointsList.Count - 1, (Vector3)_pointsList[_pointsList.Count - 1]);
 	}
 
     /// <summary>
@@ -24,7 +42,12 @@ public class DrawManager : MonoBehaviour
     /// </summary>
     public void startDraw()
     {
-        
+        _drawLine = true;
+
+        if (_pointsList != null)
+        {
+            _pointsList.Clear();
+        }
     }
 
     /// <summary>
@@ -32,6 +55,6 @@ public class DrawManager : MonoBehaviour
     /// </summary>
     public void stopDraw()
     {
-        
+        _drawLine = false;
     }
 }
